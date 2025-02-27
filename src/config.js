a631bd1dc7967638f5dcc8706fd54d34b2e68bfe
Arish <webdeveloper2@amvionlabs.com>
2025-02-27 15:23:22 +0530

    arish

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const backendPort = 280;
+export const backendPort = 80;
 export const baseURL = `${window.location.protocol}//${window.location.hostname}:${backendPort}/Gemini/`;
